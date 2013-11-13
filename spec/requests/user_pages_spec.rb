afadb3399af3a83e90f5f1858b645df71433e4f0
Dan Squires <dsquires@gmail.com>
2013-11-13 10:43:52 -0500

    Finish chapter 7 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,9 +25,41 @@ describe "User pages" do
 
     let(:submit) { "Create my account" }
 
-    describe "with invalid information" do
+    describe "with invalid (empty) information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
+      end
+
+    	describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      	it { should have_content("Name can't be blank") }
+      	it { should have_content("Email can't be blank") }
+      	it { should have_content("Email is invalid") }
+      	it { should have_content("Password can't be blank") }
+      	it { should have_content("Password is too short (minimum is 6 characters)") }
+      end
+
+    end
+
+    describe "with invalid information - different passwords" do
+      before do
+        fill_in "Password",     with: "foobar1"
+        fill_in "Confirmation", with: "foobar2"
+      end
+
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      	it { should have_content("Password confirmation doesn't match Password") }
       end
     end
 
@@ -41,6 +73,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
