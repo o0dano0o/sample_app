f659d5e70fd4070957962ec116c4238bae4a20e3
Dan Squires <dsquires@gmail.com>
2013-11-15 15:43:22 -0500

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -57,7 +57,7 @@ describe "Authentication" do
           click_button "Sign in"
         end
 
-        describe "after signing in" do
+      describe "after signing in" do
 
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
@@ -65,7 +65,7 @@ describe "Authentication" do
         end
       end
 
-       describe "in the Microposts controller" do
+      describe "in the Microposts controller" do
 
         describe "submitting to the create action" do
           before { post microposts_path }
@@ -94,7 +94,30 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_title('Sign in') }
         end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
       end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+      
     end
 
     describe "as wrong user" do
